47d578c8a7657f676d325ce2e76d6649f2aa5015
Drew Watson <tua07182@temple.edu>
2019-05-16 23:01:37 -0400

    First

---
+++ Lab2/myJavaScript.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('#nav-btn').click(function () {
+        $(this).next().toggle();
+    });
+});
+
+$(window).resize(function () {
+    if ($(window).width() > 600)
+        $('.navList').css("display", "block");
+    else
+        $('.navList').css("display", "none");
+});
